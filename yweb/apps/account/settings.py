469d5bd819dc08390076d8a0569087329e0e5e2c
Jian Li <lijian.gnu@gmail.com>
2014-05-31 20:20:17 +0800

    修改 .gitignore , 只过滤 yweb/settings.py 文件

---
+++ yweb/apps/account/settings.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from yweb.utils.translation import ugettext_lazy as _
+
+DEFAULT_CONSOLE_NAME = _('My Account')
+DEFAULT_CONSOLE_URL = '/console/account'
+
+AVATAR_MAXSIZE = 2 * 1024 * 1024 # 2M
+AVATAR_XS_SIZE = (16, 16)
+AVATAR_SM_SIZE = (24, 24)
+AVATAR_MD_SIZE = (36, 36)
+AVATAR_LG_SIZE = (120, 120)
