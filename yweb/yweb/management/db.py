a58ef7f543c840ddf9fa38ce3b54d7fb2147e61e
Jian Li <lijian.gnu@gmail.com>
2014-06-05 13:39:21 +0800

    Fix 某个 models 导入失败后被忽略的问题。

--- yweb/yweb/management/db.py
+++ yweb/yweb/management/db.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python
-# coding: utf-8
+# coding: UTF-8
 
 from yweb.conf import settings
-
+from yweb.utils.findapps import get_app_submodule
 
 def syncdb():
 
-    for m in settings.INSTALLED_APPS:
-        try:
-            exec "from %s.models import *" % m
-        except ImportError, e:
-            print 'import error: %s' % e
-            pass
+    # 保证 import models 一定成功！
+    for app_name in settings.INSTALLED_APPS:
+        models = get_app_submodule(app_name, 'models')
+        if models:
+            exec "from %s.models import *" % app_name
+        else:
+            print 'W: no models.py, pass %s' % app_name
 
     from yweb.orm import create_all
     create_all(echo=True)
